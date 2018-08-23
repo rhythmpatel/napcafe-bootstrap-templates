9e5dcc791f7a67535ca308e5678cb4c5045ded32
tui2tone <tui2tone@gmail.com>
2018-08-23 23:05:29 +0700

    [+] add font awesome

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -41,6 +41,16 @@ module.exports = {
             {
                 test: /\.(png|jpg)$/,
                 loader: 'url-loader'
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
             }
         ]
     }
